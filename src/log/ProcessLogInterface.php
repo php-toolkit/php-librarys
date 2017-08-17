252ca7da1ff731e920807fb063e608399a3e26a8
inhere <in.798@qq.com>
2017-08-17 16:21:23 +0800

    update, add log folder

---
+++ src/log/ProcessLogInterface.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: inhere
+ * Date: 2017-05-20
+ * Time: 9:49
+ */
+
+namespace inhere\library\log;
+
+/**
+ * Interface ProcessLogInterface
+ * @package inhere\library\task
+ */
+interface ProcessLogInterface
+{
+    /**
+     * Log levels can be enabled from the command line with -v, -vv, -vvv
+     */
+    const EMERG = -8;
+    const ERROR = -6;
+    const WARN = -4;
+    const NOTICE = -2;
+    const INFO = 0;
+    const PROC_INFO = 2;
+    const WORKER_INFO = 4;
+    const DEBUG = 6;
+    const CRAZY = 8;
+
+    /**
+     * Log file save type.
+     */
+    const SPLIT_NO = '';
+    const SPLIT_DAY = 'day';
+    const SPLIT_HOUR = 'hour';
+}
